81df85751f3a2502044983ef467bb654c1f42a8a
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-06-08 20:42:32 +0200

    Delete archivos innecesarios + bootstrap añadido + devolvemos layout en el App.js con variables js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const titularNoticia = "Titular de la noticia";
+  const cuerpoNoticia =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam, iusto omnis minima corrupti est officiis natus nam, adipisci commodi quo autem magnam sequi magni odio soluta temporibus velit quas!";
+  const categorias = [
+    "Noticias de deporte",
+    "Noticias de ocio",
+    "Noticias de política",
+    "Noticias de espectáculos",
+  ];
+  const otrasCosas = ["Nosequé", "Nosecuánto", "Talycual"];
+  const copyright = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="cabecera text-white row align-items-center">
+        <h1 className="col m-0">Título</h1>
+        <nav className="col text-right">
+          <ul className="links-navegacion list-inline m-0">
+            <li className="list-inline-item">hola</li>
+            <li className="list-inline-item">adiós</li>
+            <li className="list-inline-item">ciao</li>
+          </ul>
+        </nav>
       </header>
+      <div className="contenedor-principal row">
+        <main className="principal col-12 col-sm-7 col-md-8 py-4">
+          <h2>{titularNoticia}</h2>
+          <p>{cuerpoNoticia}</p>
+          <p>{cuerpoNoticia}</p>
+        </main>
+        <aside className="sidebar col-12 col-sm-5 col-md-4 py-4">
+          <section className="listado">
+            <h3>Categorías</h3>
+            <ul className="list-unstyled">
+              {categorias.map((categoria) => (
+                <li key={categoria}>{categoria}</li>
+              ))}
+            </ul>
+          </section>
+          <section className="listado">
+            <h3>Otras cosas</h3>
+            <ul className="list-unstyled">
+              {otrasCosas.map((otraCosa) => (
+                <li key={otraCosa}>{otraCosa}</li>
+              ))}
+            </ul>
+          </section>
+        </aside>
+      </div>
+      <footer className="pie text-white row align-items-center">
+        <p className="col text-center">
+          Texto del footer
+          {copyright && (
+            <span> - Ningún derecho reservado, copia lo que quieras</span>
+          )}
+        </p>
+      </footer>
     </div>
   );
 }
